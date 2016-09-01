da14237fbc66d2c3af93901d1f4c71477d8839c1
jfis <jfis.lists@gmail.com>
2016-09-01 13:38:51 -0400

    plug using generic

---
+++ apps/generic_plug/lib/plug.ex
@@ -0,0 +1,20 @@
+defmodule Plug do
+  use GenericPlug
+end
+
+defmodule Plug.Conn do
+  defstruct halted: false
+end
+
+defmodule Plug.Builder do
+  defmacro __using__(_opts) do
+    quote do
+      use GenericPlug.Builder, pluggable: Plug.Connpl
+    end
+  end
+end
+
+defimpl Pluggable, for: Plug.Conn do
+  def halted?(%Plug.Conn{halted: true}), do: true
+  def halted?(_), do: false
+end
---
+++ apps/generic_plug/lib/plug_example.ex
@@ -0,0 +1,56 @@
+defmodule ModulePlug do
+  @behaviour Plug
+
+  def init(opts) do
+    [new: "new"]
+  end
+
+  def call(conn, opts) do
+    IO.puts "module gulp"
+    IO.inspect(conn)
+    IO.inspect(opts)
+    conn
+  end
+end
+
+defmodule PlugClient do
+  use Plug.Builder
+  # use GenericPlug.Builder, pluggable: Plug.Conn
+
+  # # gulpline :qb do
+  plug :private_gulp, %{test: "eats"}
+  plug :bad
+  plug :halt
+  plug :public_gulp, 4
+  plug ModulePlug, poop: :yes
+  # # end
+  #
+  def action1() do
+    %Plug.Conn{halted: false}
+    |> call([])
+  end
+
+  #
+  defp bad(conn, _opts) do
+    2
+  end
+  defp halt(conn, _opts) do
+    %{conn | halted: true}
+  end
+
+  defp private_gulp(conn, opts) do
+    IO.puts "private gulp"
+    IO.inspect(conn)
+    IO.inspect(opts)
+    conn
+  end
+  #
+  def public_gulp(conn, opts) do
+    IO.puts "public gulp"
+    IO.inspect(conn)
+    IO.inspect(opts)
+    conn
+  end
+
+  def test(), do: "eaeaetasentas"
+end
