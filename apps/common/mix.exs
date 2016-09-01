be9630ba47f2ad986e1b05cde0473f3463370f36
jfis <jfis.lists@gmail.com>
2016-09-01 13:53:19 -0400

    example -> common

--- apps/common/mix.exs
+++ apps/common/mix.exs
@@ -1,8 +1,8 @@
-defmodule Example.Mixfile do
+defmodule Common.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :example,
+    [app: :common,
      version: "0.1.0",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
